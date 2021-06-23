ff741c00e47c13d8004d1e64e1acca85a93e8ba5
Crashcrafter <n.hack2810@gmail.com>
2021-06-23 20:29:46 +0200

    small fix

---
+++ src/main/kotlin/org/graphenedev/netty/packets/play/ChatMessageServerboundHandler.kt
@@ -0,0 +1,19 @@
+package org.graphenedev.netty.packets.play
+
+import io.netty.channel.Channel
+import org.graphenedev.mc.Server
+import org.graphenedev.netty.packets.PacketHandler
+import org.graphenedev.netty.packets.Packet
+
+class ChatMessageServerboundHandler() : PacketHandler {
+    override fun handle(channel: Channel, packet: Packet) {
+        val message = packet.readString()
+        if(message.startsWith("/")){
+            //TODO: Command
+            return
+        }
+        Server.getOnlinePlayers().forEach {
+            //TODO: Send Message to all player
+        }
+    }
+}
